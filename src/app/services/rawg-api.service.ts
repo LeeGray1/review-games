737af6ba353acc51820c25f305c7ea58b5e97ef4
Lee <s00188181@mail.itsligo.ie>
2021-04-18 22:23:02 +0100

    Fixed API

--- src/app/services/rawg-api.service.ts
+++ src/app/services/rawg-api.service.ts
@@ -1,7 +1,7 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Observable } from 'rxjs';
-import {catchError,tap} from'rxjs/operators';
+import { catchError, tap } from 'rxjs/operators';
 import { GameCollection } from 'src/app/interfaces/game'
 
 
@@ -12,19 +12,18 @@ export class RAWGApiService {
 
   private readonly allGamesUrl;
   private readonly searchGameUrl
-  
 
-  constructor( private _http: HttpClient ) { 
-    this.allGamesUrl = 'https://api.rawg.io/api/games';
-    this.searchGameUrl = 'https://api.rawg.io/api/games?page_size=1&search=';
 
+  constructor(private _http: HttpClient) {
+    this.allGamesUrl = 'https://api.rawg.io/api/games?key=3b175269f391429c90d752b37844f3c7';
+    this.searchGameUrl = 'https://api.rawg.io/api/games?key=3b175269f391429c90d752b37844f3c7&page_size=1S&search=';
   }
   public getAllGames(): Observable<GameCollection[]> {
     return this._http.get<GameCollection[]>(this.allGamesUrl);
   }
 
   public getGamesFromString(name: string): Observable<GameCollection> {
-    return this._http.get<GameCollection>(this.searchGameUrl + name );
+    return this._http.get<GameCollection>(this.searchGameUrl + name);
   }
-  
+
 }
